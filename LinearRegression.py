7f24858c910231dbcd73329e7c2c4b4b3b916360
Nagarjun Tellakula <nagarjun28@gmail.com>
2020-06-29 02:10:41 +0530

    Single variable regression using linear regression

--- LinearRegression.py
+++ LinearRegression.py
@@ -22,4 +22,16 @@ boston_df.columns = boston.feature_names
 
 boston_df['Price'] = boston.target
 
-sns.lmplot('RM','Price',data=boston_df)
+sns.lmplot('RM', 'Price', data=boston_df)
+
+X = np.vstack(boston_df.RM)
+
+Y = boston_df.Price
+
+# to write in form of a matrix to form Y=mX+c -> [X 1]
+
+X = np.array([[value, 1] for value in X], dtype=float)
+m, b = np.linalg.lstsq(X, Y, rcond=None)[0]
+plt.plot(boston_df.RM, boston_df.Price, 'o')
+x = boston_df.RM
+plt.plot(x, m * x + b, 'r', label='best fit line')
